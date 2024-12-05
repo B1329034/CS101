81cd974b67691b3217f41fd11a4005251c9e6ff1
B1329034 <jtkao.tw@gmail.com>
2024-12-05 15:30:49 +0800

    Create main2.c

---
+++ EX3/main2.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int i = 6;
+    if (i % 3 ==0){
+        printf("Love");
+    }
+    else if (i % 5 == 0){
+        printf("IU");
+    }
+    else if (i % 15 == 0){
+        printf("LOVE IU");
+    }
+    else {
+        printf("%d\n", i);
+    }
+    return 0 ;
+}    
